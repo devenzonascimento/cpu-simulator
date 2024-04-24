cd502b14db5717e900ef6071aef42c86ad22ce7b
devenzonascimento <dev.enzody@gmail.com>
2024-04-24 13:44:40 -0300

    troquei o nome da função de fechar o modal

--- src/components/Simulator/InfoModal/InfoModal.jsx
+++ src/components/Simulator/InfoModal/InfoModal.jsx
@@ -4,14 +4,14 @@ import { MdOutlineClose } from "react-icons/md";
 
 import "./styles.scss";
 
-const InfoModal = ({ isOpenInfoModal, handleCloseModal, information }) => {
+const InfoModal = ({ isOpenInfoModal, handleCloseInfoModal, information }) => {
   if (isOpenInfoModal) {
     return (
-      <div className="modal-backdrop" onClick={handleCloseModal}>
+      <div className="modal-backdrop" onClick={handleCloseInfoModal}>
         <div className="info-modal-container">
           <h1 className="info-modal-title">{information?.title}</h1>
           <p className="info-modal-body">{information?.body}</p>
-          <button className="close-button" onClick={handleCloseModal}>
+          <button className="close-button" onClick={handleCloseInfoModal}>
             <MdOutlineClose className="close-icon" />
           </button>
         </div>
