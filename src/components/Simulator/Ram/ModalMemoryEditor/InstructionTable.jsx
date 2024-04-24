3985093e4de5f1ba40109dc4318a72f64d29ecd2
devenzonascimento <dev.enzody@gmail.com>
2024-04-24 13:42:35 -0300

    refatorei o componente, rederizando os buttons atráves de um map em um array de dados

--- src/components/Simulator/Ram/ModalMemoryEditor/InstructionTable.jsx
+++ src/components/Simulator/Ram/ModalMemoryEditor/InstructionTable.jsx
@@ -1,206 +1,164 @@
+import { useState } from "react";
+
 const InstructionTable = ({ handleOpcodeClick, handleAdressClick }) => {
+  const addressButtonData = [
+    {
+      decimalValue: 0,
+      binaryValue: "0000",
+    },
+    {
+      decimalValue: 1,
+      binaryValue: "0001",
+    },
+    {
+      decimalValue: 2,
+      binaryValue: "0010",
+    },
+    {
+      decimalValue: 3,
+      binaryValue: "0011",
+    },
+    {
+      decimalValue: 4,
+      binaryValue: "0100",
+    },
+    {
+      decimalValue: 5,
+      binaryValue: "0101",
+    },
+    {
+      decimalValue: 6,
+      binaryValue: "0110",
+    },
+    {
+      decimalValue: 7,
+      binaryValue: "0111",
+    },
+    {
+      decimalValue: 8,
+      binaryValue: "1000",
+    },
+    {
+      decimalValue: 9,
+      binaryValue: "1001",
+    },
+    {
+      decimalValue: 10,
+      binaryValue: "1010",
+    },
+    {
+      decimalValue: 11,
+      binaryValue: "1011",
+    },
+    {
+      decimalValue: 12,
+      binaryValue: "1100",
+    },
+    {
+      decimalValue: 13,
+      binaryValue: "1101",
+    },
+    {
+      decimalValue: 14,
+      binaryValue: "1110",
+    },
+    {
+      decimalValue: 15,
+      binaryValue: "1111",
+    },
+  ];
+  const opcodeButtonData = [
+    {
+      opcode: "end",
+      binaryValue: "00000000",
+    },
+    {
+      opcode: "add",
+      binaryValue: "0001",
+    },
+    {
+      opcode: "sub",
+      binaryValue: "0010",
+    },
+    {
+      opcode: "store",
+      binaryValue: "0011",
+    },
+    {
+      opcode: "load",
+      binaryValue: "0101",
+    },
+    {
+      opcode: "jmp",
+      binaryValue: "0110",
+    },
+    {
+      opcode: "jmpZ",
+      binaryValue: "0111",
+    },
+    {
+      opcode: "jmpN",
+      binaryValue: "1000",
+    },
+    {
+      opcode: "input",
+      binaryValue: "10010001",
+    },
+    {
+      opcode: "output",
+      binaryValue: "10010010",
+    },
+  ];
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showAddressButtons = (value) => {
+    value.length == 4 ? setIsVisible(true) : setIsVisible(false);
+  };
+
   return (
     <div className="instructions-container">
-      <OpcodeButtons handleOpcodeClick={handleOpcodeClick}/>
-      <AdressButtons handleAdressClick={handleAdressClick}/>
+      <h2>Escolha uma instrução</h2>
+      {!isVisible && (
+        <div className="opcode-container">
+          {opcodeButtonData.map((button, index) => {
+            return (
+              <button
+                key={index}
+                className="opcode-button"
+                value={button.binaryValue}
+                onClick={({ target }) => {
+                  handleOpcodeClick(target.value);
+                  showAddressButtons(button.binaryValue);
+                }}
+              >
+                {button.opcode}
+              </button>
+            );
+          })}
+        </div>
+      )}
+      {isVisible && (
+        <div className="address-container">
+          {addressButtonData.map((button, index) => {
+            return (
+              <button
+                key={index}
+                className="address-button"
+                value={button.binaryValue}
+                onClick={({ target }) => {
+                  handleAdressClick(target.value);
+                  setIsVisible(false);
+                }}
+              >
+                {button.decimalValue}
+              </button>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
 
 export default InstructionTable;
-
-const AdressButtons = ({handleAdressClick}) => {
-  return (
-    <>
-      <button
-        className="address-button"
-        value="00000000"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        0
-      </button>
-      <button
-        className="address-button"
-        value="0001"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        1
-      </button>
-      <button
-        className="address-button"
-        value="0010"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        2
-      </button>
-      <button
-        className="address-button"
-        value="0011"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        3
-      </button>
-      <button
-        className="address-button"
-        value="0100"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        4
-      </button>
-      <button
-        className="address-button"
-        value="0101"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        5
-      </button>
-      <button
-        className="address-button"
-        value="0110"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        6
-      </button>
-      <button
-        className="address-button"
-        value="0111"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        7
-      </button>
-      <button
-        className="address-button"
-        value="1000"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        8
-      </button>
-      <button
-        className="address-button"
-        value="1001"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        9
-      </button>
-      <button
-        className="address-button"
-        value="1010"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        10
-      </button>
-      <button
-        className="address-button"
-        value="1011"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        11
-      </button>
-      <button
-        className="address-button"
-        value="1100"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        12
-      </button>
-      <button
-        className="address-button"
-        value="1101"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        13
-      </button>
-      <button
-        className="address-button"
-        value="1110"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        14
-      </button>
-      <button
-        className="address-button"
-        value="1111"
-        onClick={({ target }) => handleAdressClick(target.value)}
-      >
-        15
-      </button>
-    </>
-  );
-};
-
-const OpcodeButtons = ({handleOpcodeClick}) => {
-  return (
-    <>
-      <button
-        className="opcode-button"
-        value={"00000000"}
-        onClick={({ target }) => handleOpcodeClick(target.value)}
-      >
-        end
-      </button>
-      <button
-        className="opcode-button"
-        value={"0001"}
-        onClick={({ target }) => handleOpcodeClick(target.value)}
-      >
-        add
-      </button>
-      <button
-        className="opcode-button"
-        value={"0010"}
-        onClick={({ target }) => handleOpcodeClick(target.value)}
-      >
-        sub
-      </button>
-      <button
-        className="opcode-button"
-        value={"0011"}
-        onClick={({ target }) => handleOpcodeClick(target.value)}
-      >
-        store
-      </button>
-      <button
-        className="opcode-button"
-        value={"0101"}
-        onClick={({ target }) => handleOpcodeClick(target.value)}
-      >
-        load
-      </button>
-      <button
-        className="opcode-button"
-        value={"0110"}
-        onClick={({ target }) => handleOpcodeClick(target.value)}
-      >
-        jmp
-      </button>
-      <button
-        className="opcode-button"
-        value={"0111"}
-        onClick={({ target }) => handleOpcodeClick(target.value)}
-      >
-        jmpZ
-      </button>
-      <button
-        className="opcode-button"
-        value={"1000"}
-        onClick={({ target }) => handleOpcodeClick(target.value)}
-      >
-        jmpN
-      </button>
-      <button
-        className="opcode-button"
-        value={"10010001"}
-        onClick={({ target }) => handleOpcodeClick(target.value)}
-      >
-        input
-      </button>
-      <button
-        className="opcode-button"
-        value={"10010010"}
-        onClick={({ target }) => handleOpcodeClick(target.value)}
-      >
-        output
-      </button>
-    </>
-  );
-};
