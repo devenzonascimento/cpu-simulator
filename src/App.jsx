8f7b5420f3c6630f846bf216af0cdb1bfeb14d46
devenzonascimento <dev.enzody@gmail.com>
2024-04-24 15:03:28 -0300

    retirei funções e imports desnecessarios que haviam restado

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
-import React, { useState } from "react";
+import { CpuProvider } from "./context/CpuContext";
 
 import Settings from "./components/Settings/Settings";
 import Simulator from "./components/Simulator/Simulator";
 
-import * as Cpu from "./script/cpuScript";
-import { CpuProvider } from "./context/CpuContext";
-
 const App = () => {
-  const [memoryValue, setMemoryValue] = useState(Cpu.memory);
-
-  const handleChooseProgram = (programIndex) => {
-    Cpu.chooseProgram(programIndex);
-    updateValues();
-  };
-
-  const handleUpdateMemory = (newMemory) => {
-    setMemoryValue({ ...newMemory });
-    Cpu.updateMemory(newMemory);
-  };
-
-  const updateValues = () => {
-    handleUpdateMemory(Cpu.memory);
-    setPcValue(Cpu.pc);
-    setMarValue(Cpu.mar);
-    setMdrValue(Cpu.mdr);
-    setAccValue(Cpu.acc);
-    setCirValue(Cpu.cir);
-  };
-
   return (
     <>
       <header>
@@ -36,7 +12,7 @@ const App = () => {
       <div className="container">
         <CpuProvider>
           <Settings />
-          <Simulator UpdateMemory={handleUpdateMemory} />
+          <Simulator />
         </CpuProvider>
       </div>
     </>
