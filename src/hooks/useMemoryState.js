44cc8f7e46391b97e4e37e85ba6e9a983c78c381
devenzonascimento <dev.enzody@gmail.com>
2024-04-24 14:33:53 -0300

    Update useMemoryState.js

--- src/hooks/useMemoryState.js
+++ src/hooks/useMemoryState.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
 const useMemory = () => {
-  
-  const [memoryValue, setMemoryValue] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [memoryValue, setMemoryValue] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
 
   const updateMemory = (newMemory) => {
     setMemoryValue([...newMemory]);
