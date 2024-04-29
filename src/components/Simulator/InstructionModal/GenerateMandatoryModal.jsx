2d4e2aa14a49e0feb441d1d6423a988ef4e2c873
devenzonascimento <dev.enzody@gmail.com>
2024-04-29 12:13:54 -0300

    Refactor: fragmentei o codigo e troquei nome arquivo e pasta

---
+++ src/components/Simulator/InstructionModal/GenerateMandatoryModal.jsx
@@ -0,0 +1,39 @@
+import { createRoot } from "react-dom/client";
+
+export const waitForModal = async (ReactComponent) => {
+  return new Promise((resolve) => {
+    const modal = getModalContainer();
+
+    const onClose = (value) => {
+      removeModalFromRoot();
+      resolve(value);
+    };
+
+    createRoot(modal).render(<ReactComponent onClose={onClose} />);
+  });
+};
+
+const getModalContainer = () => {
+  const modal = createModalContainer();
+
+  appendChildInRoot(modal);
+
+  return modal;
+};
+
+const createModalContainer = () => {
+  const modal = document.createElement("div");
+  modal.classList.add("modal-backdrop");
+
+  return modal;
+};
+
+const appendChildInRoot = (containerElement) => {
+  document.getElementById("root").appendChild(containerElement);
+};
+
+const removeModalFromRoot = () => {
+  document
+    .querySelector("#root")
+    .removeChild(document.querySelector(".modal-backdrop"));
+};
