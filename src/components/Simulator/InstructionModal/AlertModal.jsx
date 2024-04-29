f296c070e46fba65992f0dc41124459c885fa200
devenzonascimento <dev.enzody@gmail.com>
2024-04-29 17:05:44 -0300

    Update AlertModal.jsx

--- src/components/Simulator/InstructionModal/AlertModal.jsx
+++ src/components/Simulator/InstructionModal/AlertModal.jsx
@@ -1,15 +1,14 @@
 import { MdOutlineClose } from "react-icons/md";
 
 const AlertModal = ({ onClose, data }) => {
-
   setTimeout(() => {
-    onClose()
+    onClose();
   }, 3000);
 
   return (
     <div className="alert-modal-container">
       <MdOutlineClose className="close-icon" onClick={onClose} />
-        <h2>{data}</h2>
+      <h2>{data}</h2>
     </div>
   );
 };
